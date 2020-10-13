5c830c5bc445678087dd16c71bb892287a6fb0b5
amaryasserAY <TokyoCode.Ay@gmail.com>
2020-10-13 19:30:52 +0200

    Finish Creat File Website

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+var gulp = require("gulp"),
+  concat = require("gulp-concat"),
+  sass = require("gulp-sass"),
+  pug = require("gulp-pug"),
+  autoprefixer = require("gulp-autoprefixer"),
+  livereload = require("gulp-livereload"),
+  sourcemaps = require("gulp-sourcemaps"),
+  minify = require("gulp-minify"),
+ notify =  require("gulp-notify");
+
+
+gulp.task("html", function () {
+  return gulp
+    .src("stage/html/*.pug")
+    .pipe(pug({ pretty: true }))
+    .pipe(gulp.dest("dist"))
+    .pipe(notify("HTML is Done"))
+    .pipe(livereload());
+});
+
+gulp.task("css", function () {
+  return gulp
+    .src(["stage/css/**/*.css", "stage/css/**/*.scss"])
+    .pipe(sourcemaps.init())
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+    .pipe(autoprefixer())
+    .pipe(concat("main.css"))
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("dist/css"))
+    .pipe(notify("CSS is Done"))
+    .pipe(livereload());
+});
+
+gulp.task("js", function () {
+  return gulp
+    .src("stage/js/*.js")
+    .pipe(sourcemaps.init())
+    .pipe(concat("main.js"))
+    .pipe(minify())
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("dist/js"))
+    .pipe(notify("JS is Done"))
+    .pipe(livereload());
+});
+
+gulp.task("watch", function () {
+  require("./server.js");
+  livereload.listen();
+  gulp.watch("stage/html/**/*.pug", ["html"]);
+  gulp.watch(["stage/css/**/*.css", "stage/css/**/*.scss"], ["css"]);
+  gulp.watch("stage/js/*.js", ["js"]);
+});
