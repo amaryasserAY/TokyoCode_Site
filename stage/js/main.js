0d148a9a64071953eceb0da27a5117efe9f0754f
amaryasserAY <TokyoCode.Ay@gmail.com>
2020-11-30 15:21:32 +0200

    Add validation form

--- stage/js/main.js
+++ stage/js/main.js
@@ -107,16 +107,94 @@ function closeFullscreen() {
 
 
 
+// =================================Form Validation===============================//
+
+  const form = document.getElementById('form');
+  const username= document.getElementById('username');
+  const email= document.getElementById('email');
+  const password = document.getElementById('password');
+  const password2 = document.getElementById('password2');
+
+
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	checkInputs();
+});
 
 
 
 
+  function 	checkInputs() {
+    const usernamevalue = username.value.trim();
+    const emailvalue = email.value.trim();
+    const passwordvalue = password.value.trim();
+    const password2value = password2.value.trim();
+
+    if (usernamevalue === '' ) {
+      
+      setErrorFor(username, 'UserName Cannot be blank');
+
+    } else {
+      setSuccesFor(username);
+    }
 
 
 
+    if (emailvalue === '') {
+      setErrorFor(email, 'Email Cannot be blank');
+      
+    } else if(!isEmail(emailvalue)){
+
+            setErrorFor(email, 'Email Is Not valid');
+      
+    } else {
+      setSuccesFor(email);
+    }
 
 
 
+        if (passwordvalue=== '' ) {
+      
+      setErrorFor(password, 'password Cannot be blank');
 
+    } else {
+      setSuccesFor(password);
+    }
+
+
+        if (password2value=== '' ) {
+      
+      setErrorFor(password2, 'password Cannot be blank');
+
+        } else if (passwordvalue !== password2value) {
+          
+setErrorFor(password2, 'Password not same password');
+
+        }else {
+      setSuccesFor(password2);
+    }
+
+
+  };
+
+
+  function  setErrorFor(input, message) {
+    const formrows = input.parentElement;
+    const small = formrows.querySelector('small');
+    small.textContent = message;
+    formrows.className = 'parent error ';
+
+  }
+
+  function setSuccesFor(input) {
+  const formrows = input.parentElement;
+      formrows.className = 'parent success ';
+}
+
+
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+  }
 
 });
